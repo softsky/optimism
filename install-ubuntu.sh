e136a3620fda970dd0efcc40db8edbb371eefaaf
Arsen A. Hutsal <aahutsal@gmail.com>
2023-10-28 13:23:10 +0300

    Update install-ubuntu.sh, added nodejs install routines

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -3,8 +3,13 @@
 # # Set the SHELL to bash with pipefail option
 # SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 ### Installation
+# nodejs
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 # updating system
-sudo apt update
+sudo apt-get update && sudo apt-get install nodejs -y
 # insatlling sudo git, curl, jq, make
 sudo apt install sudo
 sudo apt install -y git curl make jq wget bash direnv docker.io
